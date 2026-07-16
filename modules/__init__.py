f30f4fcbf89633888775b711f17015b8c7351cd2
sanjayjakhar33 <sanjayjakhar33@gmail.com>
2026-07-16 11:43:32 +0530

    Update __init__.py with package details
    Added module documentation and metadata.

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,1 +1,29 @@
-"""OCI inventory modules package."""
+"""
+OCI Inventory Generator
+
+Modules Package
+
+This package contains all inventory collectors used by
+the OCI Inventory Generator.
+
+All modules are READ ONLY and use OCI LIST/GET APIs only.
+"""
+
+__version__ = "1.0.0"
+
+__author__ = "Sanjay Jakhar"
+
+__all__ = [
+    "clients",
+    "compute",
+    "database",
+    "network",
+    "vpn",
+    "loadbalancer",
+    "storage",
+    "dns",
+    "waf",
+    "policy",
+    "excel",
+    "utils"
+]
